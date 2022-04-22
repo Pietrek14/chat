64ed408d5ed469f7fb135aa1ad8f709a3f334d23
Pietrek14 <dawidekpe@gmail.com>
2022-04-22 19:40:59 +0200

    set everything up and wrote build instruction in readme

--- frontend/scripts/tailwind.config.js
+++
@@ -1,5 +0,0 @@
-tailwind.config = {
-	theme: {
-		extend: { colors: { primary: '#1da513', gray: '#6d6d6d' } }
-	}
-}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+	  '**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+		colors: { primary: '#1da513', gray: '#6d6d6d' },
+	},
+  },
+  plugins: [],
+}

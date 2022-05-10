7c8ee329a32ba42b1c36e880bb4a8831913765d4
Pietrek14 <dawidekpe@gmail.com>
2022-05-10 17:40:11 +0200

    added constraint so that you can only send messages to your friends

--- database/setup/tables/message.sql
+++ database/setup/tables/message.sql
@@ -11,5 +11,6 @@ CREATE TABLE IF NOT EXISTS message (
 	FOREIGN KEY (author) REFERENCES user(id),
 	FOREIGN KEY (recipent) REFERENCES user(id),
 	FOREIGN KEY (reply) REFERENCES message(id),
-	CONSTRAINT CK_message_self CHECK (author <> recipent)	/* not tested, may not work */
+	CONSTRAINT CK_message_self CHECK (author <> recipent),
+	CONSTRAINT CK_message_between_friends CHECK (author IN (SELECT user1 FROM friends WHERE user2 = recipent) OR author IN (SELECT user2 FROM friends WHERE user1 = recipent))
 );

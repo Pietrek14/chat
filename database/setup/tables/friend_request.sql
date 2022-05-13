833cfbd78e79b66989d25cad6c093222bf11a01a
imustend <plochockitymon@gmail.com>
2022-05-13 20:26:38 +0200

    david's sql changes  and check if you already invited friend before sending them an invitation

--- database/setup/tables/friend_request.sql
+++ database/setup/tables/friend_request.sql
@@ -1,15 +1,15 @@
 USE `chat`;
 
 CREATE TABLE friend_request (
-	requester INT NOT NULL,
-	adressee INT NOT NULL,
-	code CHAR(16) NOT NULL,
-	creation_date DATETIME NOT NULL,
-	lesser_user INT GENERATED ALWAYS AS (CASE WHEN requester < adressee THEN requester ELSE adressee END),
-	greater_user INT GENERATED ALWAYS AS (CASE WHEN requester < adressee THEN adressee ELSE requester END),
-	PRIMARY KEY (requester, adressee),
-	FOREIGN KEY (requester) REFERENCES user(id),
-	FOREIGN KEY (adressee) REFERENCES user(id),
-	CONSTRAINT CK_friendship_self CHECK (requester <> adressee),
-	CONSTRAINT UQ_friendship_users_ordered UNIQUE (lesser_user, greater_user)
+    requester INT NOT NULL,
+    adressee INT NOT NULL,
+    code CHAR(16) NOT NULL,
+    creation_date DATETIME NOT NULL,
+    lesser_user INT GENERATED ALWAYS AS (CASE WHEN requester < adressee THEN requester ELSE adressee END),
+    greater_user INT GENERATED ALWAYS AS (CASE WHEN requester < adressee THEN adressee ELSE requester END),
+    PRIMARY KEY (requester, adressee),
+    FOREIGN KEY (requester) REFERENCES user(id),
+    FOREIGN KEY (adressee) REFERENCES user(id),
+    CONSTRAINT CK_friend_request_self CHECK (requester <> adressee),
+    CONSTRAINT UQ_friend_request_users_ordered UNIQUE (lesser_user, greater_user)
 );
